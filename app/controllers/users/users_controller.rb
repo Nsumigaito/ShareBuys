e8b992d45115a72a87ba6b74fc98319ada643b63
vagrant <vagrant@localhost.localdomain>
2020-10-14 09:45:22 +0000

    [Add] マイページ　ユーザー部分だけ完成

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,5 +1,11 @@
 class Users::UsersController < ApplicationController
 	def show
-		
+		@user = User.find(params[:id])
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduce, :image,
+										:point, :telephone, :is_deleted, :is_admin)
 	end
 end
