4296bc7d79ed8480eb527e897ee990a205c7c0ce
vagrant <vagrant@localhost.localdomain>
2020-10-20 09:09:01 +0000

    [Add] 検索機能　仮完成

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,2 +1,12 @@
 class Users::SearchesController < ApplicationController
+	def search
+		@word = params[:word]
+
+		if @word == nil
+			redirect_to request.referrer
+		else
+			@users = User.search(@word)
+			@posts = Post.search(@word)
+		end
+	end
 end
