6163cc0e2d94569d58f03c0d8f22349fcc843290
vagrant <vagrant@localhost.localdomain>
2020-10-20 04:10:16 +0000

    [Add] いいね機能　仮完成

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   belongs_to :user
 
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
