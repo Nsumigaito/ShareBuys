97f294434c99910d79b74bbeea42e0f928465a82
vagrant <vagrant@localhost.localdomain>
2020-12-28 09:10:58 +0000

    [Add] about How to page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_021056) do
+ActiveRecord::Schema.define(version: 2020_12_10_055000) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
